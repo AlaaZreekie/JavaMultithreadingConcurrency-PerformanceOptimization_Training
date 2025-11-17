cc3bbf2a75b9aee52eec04da1afa73533de847c6
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2025-11-17 18:34:15 +0300

    Call interrupt with long coputation tasks to stop them senario

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -5,12 +6,11 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new BlockingTask());
-        System.out.println("Starting thread");
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("20000000"), new BigInteger("33333333333")));
+
         thread.start();
-        System.out.println("Waiting for thread");
+        //this will stop the thread from working for long long time
         thread.interrupt();
-        System.out.println("Thread interrupted");
     }
 
     private static class BlockingTask implements Runnable {
@@ -24,5 +24,35 @@ public class Main {
             }
 
         }
+    }
+
+    private static class LongComputationTask implements Runnable {
+
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base + "^" + power + " = " + pow(base, power));
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for(BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                //calling the interrupt method is not enough so we have to make sure that the hot spot in the code can be interrupted
+                if(Thread.currentThread().isInterrupted()) {
+                    System.out.println(Thread.currentThread().getName() + " interrupted");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+            return result;
+        };
     }
 }
