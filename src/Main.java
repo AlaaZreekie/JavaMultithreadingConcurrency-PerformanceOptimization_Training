fb3b792cdfa15b844c7f68df06b58927aa6eebe9
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2025-11-17 18:47:35 +0300

    Init Thread Coordination senario with factorial example

--- src/Main.java
+++ src/Main.java
@@ -6,56 +6,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new LongComputationTask(new BigInteger("20000000"), new BigInteger("33333333333")));
 
-        //this makes even if the long calculation did not end, it closes when the main thread ended makes the app end
-         thread.setDaemon(true);
-        //
-        thread.start();
-        //this will stop the thread from working for long long time
-        thread.interrupt();
     }
 
-    private static class BlockingTask implements Runnable {
-        @Override
-        public void run() {
-            //do things
-            try {
-                Thread.sleep(500000);
-            }catch (InterruptedException e){
-                System.out.println(e + " interrupted");
-            }
-
-        }
-    }
-
-    private static class LongComputationTask implements Runnable {
-
-        private BigInteger base;
-        private BigInteger power;
-
-        public LongComputationTask(BigInteger base, BigInteger power) {
-            this.base = base;
-            this.power = power;
-        }
-
-        @Override
-        public void run() {
-            System.out.println(base + "^" + power + " = " + pow(base, power));
-        }
-
-        private BigInteger pow(BigInteger base, BigInteger power) {
-            BigInteger result = BigInteger.ONE;
-
-            for(BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
-                //calling the interrupt method is not enough so we have to make sure that the hot spot in the code can be interrupted
-                if(Thread.currentThread().isInterrupted()) {
-                    System.out.println(Thread.currentThread().getName() + " interrupted");
-                    return BigInteger.ZERO;
-                }
-                result = result.multiply(base);
-            }
-            return result;
-        };
-    }
 }
