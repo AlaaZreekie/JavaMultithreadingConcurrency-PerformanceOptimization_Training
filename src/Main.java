2a6dcfec310045f1f35933aadd3e36d4ea726b5e
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2025-11-17 17:39:55 +0300

    Init Case study vault hacking senario

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,11 @@
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run(){
-                //Code that will run in a new thread
-                System.out.println("We are now on the thread: "+Thread.currentThread().getName());
-                System.out.println(Thread.currentThread().getPriority());
-                throw new RuntimeException("Interrupted");
-            }
-        });
+    public static void main(String[] args) {
 
 
-        thread.setName("New Worker Thread");
-        thread.setPriority(Thread.MAX_PRIORITY);
 
-        System.out.println("We are in thread: " + Thread.currentThread().getName()+ " before staring new thread");
-        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                System.out.println("Uncaught exception in thread: "+Thread.currentThread().getName()+ " "+e);
-            }
-        });
-        thread.start();
-        System.out.println("We are in thread: " + Thread.currentThread().getName()+ " after staring new thread");
-
-        Thread newThread = new NewThread();
-        newThread.setName("New Thread");
-        newThread.start();
-        Thread.sleep(10000);
     }
 
-    private static class NewThread extends Thread{
-        @Override
-        public void run(){
-            System.out.println("We are in thread: "+Thread.currentThread().getName() + ", Hello World From New Thread");
-        }
-    }
+
 }
