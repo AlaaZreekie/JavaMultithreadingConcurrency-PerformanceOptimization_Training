d07e8311971bc9edf9a080fff312cbee91cf4698
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2025-11-17 18:20:27 +0300

    First Senario to how to use interrupt when thread use methods that throw exception

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,24 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        Thread thread = new Thread(new BlockingTask());
+        System.out.println("Starting thread");
+        thread.start();
+        System.out.println("Waiting for thread");
+        thread.interrupt();
+        System.out.println("Thread interrupted");
     }
 
+    private static class BlockingTask implements Runnable {
+        @Override
+        public void run() {
+            //do things
+            try {
+                Thread.sleep(500000);
+            }catch (InterruptedException e){
+                System.out.println(e + " interrupted");
+            }
+
+        }
+    }
 }
