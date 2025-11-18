178c8926a4d5b35c16e7175a7a6546d3f2caabd1
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2025-11-18 06:09:55 +0300

    Implementing example explain sharing resources danger

--- src/race/condition/Main.java
+++ src/race/condition/Main.java
@@ -4,14 +4,48 @@ import java.io.IOException;
 
 public class Main {
     static void main(String [] args) throws InterruptedException, IOException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
 
+        incrementThread.start();
+        incrementThread.join();
+
+        decrementingThread.start();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
     }
 
-    public static class IncrementThread extends Thread {
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter counter;
+        public IncrementingThread(InventoryCounter counter) {
+            this.counter = counter;
+        }
 
+        @Override
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        }
     }
 
-    private static class InverntoryCounter {
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter counter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                counter.decrement();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
         private int items = 0;
         public void increment() {
             items++;
